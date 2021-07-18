586489455f246014d29a8f6193781587d6a9023c
LOGESHWARAN S <logeshwaran@mobax.com>
2021-07-18 19:53:53 +0530

    Added logic and complete first phase coding done.

--- Scheduler/CreateEventController.swift
+++ Scheduler/CreateEventController.swift
@@ -9,10 +9,31 @@ import UIKit
 
 class CreateEventController: UIViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var eventNameText: UITextField!
+	@IBOutlet weak var datePicker: UIDatePicker!
+	
+	// data object
+	var event: Event? // default value nil
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		
+		eventNameText.delegate = self
+		
+		event = Event(date: Date(), name: "default value")
     }
 
+	@IBAction func didTapDatePicker(_ sender: UIDatePicker) {
+		event?.date = sender.date
+	}
+	
+}
+
+extension CreateEventController: UITextFieldDelegate {
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		// to update event name
+		event?.name = textField.text ?? "no event name enter"
+		return true
+	}
 }
--- Scheduler/ScheduleListController.swift
+++ Scheduler/ScheduleListController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ScheduleListController: UIViewController {
 
+	@IBOutlet weak var tableView: UITableView!
+	
+	var events = [Event]()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		tableView.dataSource = self
 	}
 
+	@IBAction func addNewEvent(sender: UIStoryboardSegue) {
+		
+		guard let createEventController = sender.source as? CreateEventController,
+			  let createEvent = createEventController.event else {
+			fatalError("Fail to load create eventcontroller")
+		}
+		
+		events.insert(createEvent, at: 0)
+		
+		let indexPath = IndexPath(item: 0, section: 0)
+		
+		tableView.insertRows(at: [indexPath], with: .automatic)
+		
+	}
 
+}
+
+extension ScheduleListController: UITableViewDataSource {
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return events.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "eventCell", for: indexPath)
+		
+		let eventValue = events[indexPath.row]
+		cell.textLabel?.text = eventValue.name
+		cell.detailTextLabel?.text = eventValue.date.description
+		
+		return cell
+	}
+	
 }
 
